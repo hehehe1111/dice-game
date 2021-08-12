4f75d0182dce20e43e64143bef5d870591621e0e
hemantafpv <87780753+hemantafpv@users.noreply.github.com>
2021-08-12 11:56:39 -0400

    Add files via upload
    dice game made using three different types of javascript codes.

---
+++ index3.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage1="images/dice" + randomNumber1 + ".png";
+var randomDiceImage2="images/dice" + randomNumber2 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+image1.setAttribute("src",randomDiceImage1);
+image2.setAttribute("src",randomDiceImage2);
+
+if (randomNumber1 > randomNumber2) {
+document.querySelector(".container h1").innerText = "🚩 Player 1 wins!";}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector(".container h1").innerText = "Player 2 wins! 🚩";}
+
+  else document.querySelector(".container h1").innerText = "Draw!";
